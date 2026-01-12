fee630cc10f31e59c33bcc96119e36eaf90a513f
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-12 22:38:57 +0530

    code update

--- temperature.cpp
+++ temperature.cpp
@@ -4,6 +4,10 @@ int main() {
     std::cout << "Enter the temperature in fahrenheit: \n";
     double fahrenheit;
     std::cin>> fahrenheit;
-    
+
+    std::cout << "The temperature in celsius is: \n";
+    double celsius = (fahrenheit - 32) / 1.8;
+
+    std::cout << celcius << "\n";
 
 }
