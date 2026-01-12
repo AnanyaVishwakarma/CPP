ad86f1aa0c3c7070e5f19b0a12812c222872dbd6
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-12 22:27:39 +0530

    conditions added

--- coin.cpp
+++ coin.cpp
@@ -5,5 +5,9 @@ int main (){
     srand(time(NULL));
 
     int num = rand() % 2; // Generates either 0 or 1; Random
-
-}
+    if(num > 0.5){
+        std::cout << "Heads\n";
+    } else {
+        std::cout << "Tails\n";
+    }
+    }
