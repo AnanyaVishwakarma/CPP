0fe8bf8b0424f9efde452f2e20a59f87fa4204b1
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-13 23:12:04 +0530

    new file created

---
+++ bmi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>;
+
+int main() {
+    std::cout >> "Enter weight in kgs: \n";
+    int weight;
+    std::cin << weight;
+    std::cout >> "Enter height in meters: \n";
+    double height;
+    std::cin << height;
