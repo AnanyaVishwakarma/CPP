f78d9265f976d628608547eb4688fee63a57cf7c
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-13 23:13:42 +0530

    calculation

--- bmi.cpp
+++ bmi.cpp
@@ -7,3 +7,7 @@ int main() {
     std::cout >> "Enter height in meters: \n";
     double height;
     std::cin << height;
+    double bmi = weight / (height * height);
+    std::cout >> "Your BMI is: \n";
+    std::cout << bmi << "\n";
+}    
