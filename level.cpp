fa22c0a4349ba254ee07473aaa6e3e0c6b399ce5
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-14 22:55:07 +0530

    level.cpp initialized

--- level.cpp
+++ level.cpp
@@ -3,4 +3,16 @@
 
 int main () {
     int level = 3; 
+      if(level <= 5){
+    std::cout << "Bronze \n";
+  } else if(level <= 10){
+    std::cout << "Silver \n";
+  } else if(level  <= 15){
+    std::cout << "Gold \n";
+  } else if(level <= 20) {
+    std::cout << "Platinum \n";
+  } else {
+    std::cout << "Diamond \n";
+  }
+ 
 }
