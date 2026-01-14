6c0ee65b69534abcadd93ce505f4d5bdb5d9a98a
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-14 23:04:08 +0530

    conditions

--- fortuneCookies.cpp
+++ fortuneCookies.cpp
@@ -2,5 +2,22 @@
 #include <cstdlib>
 
 int main () {
-    
+  srand(time(NULL));
+   int random = std::rand() % 6;
+   std::cout << random;
+
+   if(random == 1){
+     std::cout << "Don't pursue happiness – create it";
+   } else if(random == 2){
+     std::cout << "All things are difficult before they are easy.";
+   } else if(random == 3){
+     std::cout << "The early bird gets the worm, but the second mouse gets the cheese";
+   } else if(random == 4){
+     std::cout << "Someone in your life needs a letter from you";
+   } else if(random == 5){
+     std::cout << "The fortune you search for is in another cookie.";
+   } else {
+     std::cout << "Help! I'm being held prisoner in a Chinese bakery!";
+   } 
+
 }
