21b5af150aa69c2b293207ee479c8dca6eb5b2f6
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-14 23:12:29 +0530

    destination weight calculated

--- PlanetWeight.cpp
+++ PlanetWeight.cpp
@@ -3,4 +3,7 @@
 
 int main (){
     double earth_weight = 57.6;
+    std::cout << "Your weight on Earth is: " << earth_weight << " kgs\n";
+    double mars_weight = earth_weight * 0.38;
+    std::cout << "Your weight on Mars is: " << mars_weight << " kgs\n";
 }
