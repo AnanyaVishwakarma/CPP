e242eb73f3f81b2647d7284db78a86213ce03182
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-21 00:01:30 +0530

    Questions made

--- triviaQuiz.cpp
+++ triviaQuiz.cpp
@@ -1,5 +1,12 @@
 #include <stdio>
 
-int main() {
-    
+int main()
+{
+    std::string answer1;
+    std::cout << "💧 What is the name of the world's longest river?\n";
+    std::cout << "1) Missouri River\n";
+    std::cout << "2) Nile\n";
+    std::cout << "3) Amazon River\n";
+    std::cout << "4) Yangtze River\n";
+
 }
