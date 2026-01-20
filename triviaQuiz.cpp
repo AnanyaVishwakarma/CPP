173456a96cdf9349b713fa66ead8cb480f0b8b62
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-21 00:04:46 +0530

    Condition made

--- triviaQuiz.cpp
+++ triviaQuiz.cpp
@@ -8,5 +8,13 @@ int main()
     std::cout << "2) Nile\n";
     std::cout << "3) Amazon River\n";
     std::cout << "4) Yangtze River\n";
-
+    std::cin >> answer1;
+    if (answer1 == "2")
+    {
+        std::cout << "You're right";
+    }
+    else
+    {
+        std::cout << "Choose Again";
+    }
 }
